028121afe65ac20b760c4457a40e8247947cfd83
Ken Lu <ken@seeice.com>
2013-04-16 08:50:20 -0400

    second update for map and reduce job

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,19 +1,28 @@
 class Band
   include Mongoid::Document
   field :name, type: String
-  field :likes, type: Integer
+  field :likes, type: Integer, default: 0
+  field :foo, type: Boolean, default: false
+  field :bar, type: Boolean, default: false
+
   def self.mar
     map = %Q{
       function() {
-        emit(this.name, { likes: this.likes });
+        hsh = {};
+        if(this.foo) {hsh.foo = this.foo}
+        if(this.bar) {hsh.bar = this.bar}
+        emit(this.name, { likes: this.likes, hh: hsh });
       }
     }
 
     reduce = %Q{
       function(key, values) {
-        var result = { likes: 0 };
+        var result = { likes: 0, hh: 0 };
         values.forEach(function(value) {
           result.likes += value.likes;
+          if(value.hh.foo) {result.hh += 1}
+          if(value.hh.bar) {result.hh += 1}
+
         });
         return result;
       }
