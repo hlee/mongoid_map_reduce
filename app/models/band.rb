f643b6659aa938d9772dca30b184fd9345ec00a2
Ken Lu <ken@seeice.com>
2013-04-16 08:50:20 -0400

    added function map reduce

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,4 +2,23 @@ class Band
   include Mongoid::Document
   field :name, type: String
   field :likes, type: Integer
+  def self.mar
+    map = %Q{
+      function() {
+        emit(this.name, { likes: this.likes });
+      }
+    }
+
+    reduce = %Q{
+      function(key, values) {
+        var result = { likes: 0 };
+        values.forEach(function(value) {
+          result.likes += value.likes;
+        });
+        return result;
+      }
+    }
+
+    self.where(:likes.gt => 100).map_reduce(map, reduce).out(inline: true)
+  end
 end
